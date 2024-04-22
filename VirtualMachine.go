e36f1381b2ee5fae673b881d62df3c0ec83554e7
Maros Meciar <maros.mec@seznam.cz>
2024-04-22 12:39:51 +0200

    feat: project done

--- VirtualMachine.go
+++ VirtualMachine.go
@@ -13,42 +13,42 @@ type VM struct {
 	variables map[string]Variable
 }
 
-func (s *VM) Push(item interface{}) {
-	s.data = append(s.data, item)
+func (vm *VM) Push(item interface{}) {
+	vm.data = append(vm.data, item)
 }
 
-func (s *VM) Pop() interface{} {
-	if len(s.data) == 0 {
+func (vm *VM) Pop() interface{} {
+	if len(vm.data) == 0 {
 		return nil
 	}
-	item := s.data[len(s.data)-1]
-	s.data = s.data[:len(s.data)-1]
+	item := vm.data[len(vm.data)-1]
+	vm.data = vm.data[:len(vm.data)-1]
 	return item
 }
 
-func (s *VM) PopTwo() (interface{}, interface{}) {
-	if len(s.data) < 2 {
+func (vm *VM) PopTwo() (interface{}, interface{}) {
+	if len(vm.data) < 2 {
 		return nil, nil
 	}
-	b := s.data[len(s.data)-1]
-	a := s.data[len(s.data)-2]
-	s.data = s.data[:len(s.data)-2]
+	b := vm.data[len(vm.data)-1]
+	a := vm.data[len(vm.data)-2]
+	vm.data = vm.data[:len(vm.data)-2]
 	return a, b
 }
 
-func (s *VM) PrintVM() {
+func (vm *VM) PrintVM() {
 	fmt.Printf("[")
-	for i := 0; i < len(s.data); i++ {
-		fmt.Printf("%v", s.data[i])
-		if i < len(s.data)-1 {
+	for i := 0; i < len(vm.data); i++ {
+		fmt.Printf("%v", vm.data[i])
+		if i < len(vm.data)-1 {
 			fmt.Printf(", ")
 		}
 	}
 	fmt.Printf("]\n")
 }
 
-func (s *VM) IsEmpty() bool {
-	return len(s.data) == 0
+func (vm *VM) IsEmpty() bool {
+	return len(vm.data) == 0
 }
 
 func splitInstruction(instruction string) []string {
@@ -75,17 +75,17 @@ func splitInstruction(instruction string) []string {
 	return parts
 }
 
-func (s *VM) Evaluate(instructions []string) {
-	s.data = make([]interface{}, 0)
-	s.labels = make(map[int]int)
-	s.variables = make(map[string]Variable)
+func (vm *VM) Evaluate(instructions []string) {
+	vm.data = make([]interface{}, 0)
+	vm.labels = make(map[int]int)
+	vm.variables = make(map[string]Variable)
 
 	for i := 0; i < len(instructions); i++ {
 		parts := splitInstruction(instructions[i])
 		opcode := parts[0]
 		if opcode == "label" {
 			n, _ := strconv.Atoi(parts[1])
-			s.labels[n] = i
+			vm.labels[n] = i
 		}
 	}
 	for i := 0; i < len(instructions); i++ {
@@ -98,21 +98,21 @@ func (s *VM) Evaluate(instructions []string) {
 			switch parts[1] {
 			case "I":
 				intValue, _ := strconv.Atoi(value)
-				s.Push(intValue)
+				vm.Push(intValue)
 			case "F":
 				floatValue, _ := strconv.ParseFloat(value, 64)
-				s.Push(floatValue)
+				vm.Push(floatValue)
 			case "S":
-				s.Push(value)
+				vm.Push(value)
 			case "B":
 				boolValue, _ := strconv.ParseBool(value)
-				s.Push(boolValue)
+				vm.Push(boolValue)
 			}
 		case "print":
 			n, _ := strconv.Atoi(parts[1])
 			prints := make([]string, n)
 			for i := 0; i < n; i++ {
-				str := fmt.Sprintf("%v", s.Pop())
+				str := fmt.Sprintf("%v", vm.Pop())
 				if str[0] == '"' {
 					str = str[1 : len(str)-1]
 				}
@@ -123,25 +123,25 @@ func (s *VM) Evaluate(instructions []string) {
 			}
 			println()
 		case "pop":
-			s.Pop()
+			vm.Pop()
 		case "save":
 			id := parts[1]
-			s.variables[id] = Variable{id: id, value: s.Pop()}
+			vm.variables[id] = Variable{id: id, value: vm.Pop()}
 		case "load":
 			id := parts[1]
-			s.Push(s.variables[id].value)
+			vm.Push(vm.variables[id].value)
 		case "label":
 		case "jmp":
 			n, _ := strconv.Atoi(parts[1])
-			labelIdx, ok := s.labels[n]
+			labelIdx, ok := vm.labels[n]
 			if !ok {
 				panic(fmt.Sprintf("Label %d not found", n))
 			}
 			i = labelIdx
 		case "fjmp":
 			n, _ := strconv.Atoi(parts[1])
-			if !s.Pop().(bool) {
-				labelIdx, ok := s.labels[n]
+			if !vm.Pop().(bool) {
+				labelIdx, ok := vm.labels[n]
 				if !ok {
 					panic(fmt.Sprintf("Label %d not found", n))
 				}
@@ -152,117 +152,117 @@ func (s *VM) Evaluate(instructions []string) {
 			fmt.Printf("Enter value: ")
 			_, err := fmt.Scan(&input)
 			if err != nil {
-				panic(fmt.Sprintf("Error reading input: %s", err))
+				panic(fmt.Sprintf("Error reading input: %vm", err))
 			}
 			switch parts[1] {
 			case "I":
 				intValue, ok := strconv.Atoi(input)
 				if ok != nil {
-					panic(fmt.Sprintf("Error parsing int value: %s", ok))
+					panic(fmt.Sprintf("Error parsing int value: %vm", ok))
 				}
-				s.Push(intValue)
+				vm.Push(intValue)
 			case "F":
 				floatValue, ok := strconv.ParseFloat(input, 64)
 				if ok != nil {
-					panic(fmt.Sprintf("Error parsing float value: %s", ok))
+					panic(fmt.Sprintf("Error parsing float value: %vm", ok))
 				}
-				s.Push(floatValue)
+				vm.Push(floatValue)
 			case "S":
-				s.Push(input)
+				vm.Push(input)
 			case "B":
 				boolValue, ok := strconv.ParseBool(input)
 				if ok != nil {
-					panic(fmt.Sprintf("Error parsing boolean value: %s", ok))
+					panic(fmt.Sprintf("Error parsing boolean value: %vm", ok))
 				}
-				s.Push(boolValue)
+				vm.Push(boolValue)
 			}
 		case "itof":
-			s.Push(float64(s.Pop().(int)))
+			vm.Push(float64(vm.Pop().(int)))
 		case "add":
-			a, b := s.PopTwo()
+			a, b := vm.PopTwo()
 			switch a.(type) {
 			case int:
-				s.Push(a.(int) + b.(int))
+				vm.Push(a.(int) + b.(int))
 			case float64:
-				s.Push(a.(float64) + b.(float64))
+				vm.Push(a.(float64) + b.(float64))
 			}
 		case "sub":
-			a, b := s.PopTwo()
+			a, b := vm.PopTwo()
 			switch a.(type) {
 			case int:
-				s.Push(a.(int) - b.(int))
+				vm.Push(a.(int) - b.(int))
 			case float64:
-				s.Push(a.(float64) - b.(float64))
+				vm.Push(a.(float64) - b.(float64))
 			}
 		case "mul":
-			a, b := s.PopTwo()
+			a, b := vm.PopTwo()
 			switch a.(type) {
 			case int:
-				s.Push(a.(int) * b.(int))
+				vm.Push(a.(int) * b.(int))
 			case float64:
-				s.Push(a.(float64) * b.(float64))
+				vm.Push(a.(float64) * b.(float64))
 			}
 		case "div":
-			a, b := s.PopTwo()
+			a, b := vm.PopTwo()
 			switch a.(type) {
 			case int:
-				s.Push(a.(int) / b.(int))
+				vm.Push(a.(int) / b.(int))
 			case float64:
-				s.Push(a.(float64) / b.(float64))
+				vm.Push(a.(float64) / b.(float64))
 			}
 		case "mod":
-			a, b := s.PopTwo()
-			s.Push(a.(int) % b.(int))
+			a, b := vm.PopTwo()
+			vm.Push(a.(int) % b.(int))
 		case "uminus":
-			a := s.Pop()
+			a := vm.Pop()
 			switch a.(type) {
 			case int:
-				s.Push(-a.(int))
+				vm.Push(-a.(int))
 			case float64:
-				s.Push(-a.(float64))
+				vm.Push(-a.(float64))
 			}
 		case "not":
-			a := s.Pop()
-			s.Push(!a.(bool))
+			a := vm.Pop()
+			vm.Push(!a.(bool))
 		case "concat":
-			b := s.Pop().(string)
-			a := s.Pop().(string)
+			b := vm.Pop().(string)
+			a := vm.Pop().(string)
 			a = a[1 : len(a)-1]
 			b = b[1 : len(b)-1]
-			s.Push(a + b)
+			vm.Push(a + b)
 		case "and":
-			b := s.Pop().(bool)
-			a := s.Pop().(bool)
-			s.Push(a && b)
+			b := vm.Pop().(bool)
+			a := vm.Pop().(bool)
+			vm.Push(a && b)
 		case "or":
-			b := s.Pop().(bool)
-			a := s.Pop().(bool)
-			s.Push(a || b)
+			b := vm.Pop().(bool)
+			a := vm.Pop().(bool)
+			vm.Push(a || b)
 		case "gt":
-			b := s.Pop()
-			a := s.Pop()
+			b := vm.Pop()
+			a := vm.Pop()
 			switch a.(type) {
 			case int:
-				s.Push(a.(int) > b.(int))
+				vm.Push(a.(int) > b.(int))
 			case float64:
-				s.Push(a.(float64) > b.(float64))
+				vm.Push(a.(float64) > b.(float64))
 			}
 		case "lt":
-			b := s.Pop()
-			a := s.Pop()
+			b := vm.Pop()
+			a := vm.Pop()
 			switch a.(type) {
 			case int:
-				s.Push(a.(int) < b.(int))
+				vm.Push(a.(int) < b.(int))
 			case float64:
-				s.Push(a.(float64) < b.(float64))
+				vm.Push(a.(float64) < b.(float64))
 			}
 		case "eq":
-			b := s.Pop()
-			a := s.Pop()
-			s.Push(a == b)
+			b := vm.Pop()
+			a := vm.Pop()
+			vm.Push(a == b)
 		}
 	}
-	if !s.IsEmpty() {
+	if !vm.IsEmpty() {
 		panic("VM is not empty")
 	}
 }
